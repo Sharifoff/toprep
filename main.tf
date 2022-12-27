f5ba444c6a78cc1e4a0cad52de3111bddaa429b4
Sharifoff <efqanserifov@gmail.com>
2022-12-27 10:51:46 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+
+  backend "s3" {
+    bucket = "soccar081222"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-1"
+  }
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_default_vpc" "default" {
+    tags = {
+      Name = "Default VPC"
+    }
+  }
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0b0dcb5067f052a63"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "AppServer08122022"
+
+  }
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "soccar081222"
+
+ 
+}
+
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
